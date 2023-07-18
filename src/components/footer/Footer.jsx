4272ff77e0f5a4c1bdd6bbd995f3b02e23442a7c
Ali <AliNazarii7394@gmail.com>
2023-07-18 20:53:54 +0330

    layout & components done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div>
+        <div>©2023 Lamamia. All rights reserved.</div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
