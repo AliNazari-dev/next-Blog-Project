4049b97ffd8123d13bf1feafd7134e56143cbdf3
Ali <AliNazarii7394@gmail.com>
2023-07-23 11:55:31 +0330

    add and delet post done

--- src/components/navabar/Navbar.jsx
+++ src/components/navabar/Navbar.jsx
@@ -1,7 +1,10 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import styles from "./navbar.module.css";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { signOut, useSession } from "next-auth/react";
+
 
 const links = [
   {
@@ -36,22 +39,29 @@ const links = [
   },
 ];
 
-function Navbar() {
+const Navbar = () => {
+  const session = useSession();
+
   return (
     <div className={styles.container}>
-      <Link className={styles.logo} href={"/"}>
-        lamadev
+      <Link href="/" className={styles.logo}>
+        lamamia
       </Link>
       <div className={styles.links}>
         <DarkModeToggle />
         {links.map((link) => (
-          <Link className={styles.link} href={link.url} key={link.id}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
